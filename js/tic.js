92ae533c22e8e8e7e68d8fa9b5cc9e2a539fffe3
Vardaan100 <vardaanmagon@yahoo.com>
2020-11-30 13:13:49 +0530

    Updated Tictactoe.js

--- js/tic.js
+++ js/tic.js
@@ -1,48 +1,49 @@
 const boxes=Array.from(document.getElementsByClassName("box"));
-const play=document.getElementById("play");//win message
+const play=document.getElementById("play");
 const restartBtn=document.getElementById("restartBtn");
-const spaces=[];//if something is in the box we dont want user to click on it
+const spaces=[];
 const O_TEXT='0';
 const X_TEXT='X';
 var count=0;
 let currentPlayer; 
-console.log(boxes);
-const drawBoard=()=>{ //used to draw board 
+let gameLive = true;
+const drawBoard=()=>{ 
     boxes.forEach((box, index) => { //taking each box and index of each box
         let styleString=''; //to add borders we take empty styleString
-        if(index<3){ //box is on the top 
-            styleString+=`border-bottom:3px solid var(--purple);`;//sets bottom line
+        if(index<3) { //box is on the top 
+            styleString+=`border-bottom:3px solid var(--purple);`;
         }
-        if(index%3===0){//we are on left
-            styleString+=`border-right:3px solid var(--purple);`;//sets the right line
+        if(index%3===0) {//we are on left
+            styleString+=`border-right:3px solid var(--purple);`;
         }
-        if(index%3===2){//we are on right
-            styleString+=`border-left:3px solid var(--purple);`;//sets the left line
+        if(index%3===2) {//we are on right
+            styleString+=`border-left:3px solid var(--purple);`;
         }
-        if(index>5){ //box is at the bottom
-            styleString+=`border-top:3px solid var(--purple);`;//sets the top line
+        if(index>5) { //box is at the bottom
+            styleString+=`border-top:3px solid var(--purple);`;
         }
         box.style=styleString; 
         box.addEventListener('click',boxClicked);//whether box is clicked/not
     });
 };
-const boxClicked=(e)=> {//adding boxclicked function
+const boxClicked=(e)=> {
     const id=e.target.id;//getting id of the element
     count++;//increments the count value
     console.log("count"+count)//counting the number of times box have been clicked
     
-    if(!spaces[id]) {//if nothing in spaces
+    if(!spaces[id] && gameLive == true) {//if nothing in spaces
         spaces[id]=currentPlayer;
         e.target.innerText=currentPlayer;//setting inner box to current player
         
         if(playerHasWon()){//handling if player has won
+            gameLive = false;
             play.innerText=`${currentPlayer} has won!`;
             return;
         }
-        if(!spaces.includes(null) && count==9){ //if all spaces are filled and count is 9 returns draw
-            play.innerText="Draw";
+        if(count==9){ //if count is 9 returns draw
+            play.innerText="Draw!";
         }
-        currentPlayer=currentPlayer===O_TEXT ? X_TEXT : O_TEXT; /*if current player=O then  X otherwise we set O */
+        currentPlayer=currentPlayer===O_TEXT ? X_TEXT : O_TEXT; /*if current player=O then X otherwise we set O */
         
     }
 
@@ -95,12 +96,15 @@ const playerHasWon=()=>{ //checking if player has won (conditions)
 const restart =()=>{
     spaces.forEach((space,index)=>{ //wipe out each space
         spaces[index]=null;
+        count=0;
     });
     boxes.forEach(box=>{
         box.innerText='';//wipe out text 
     });
     play.innerText="Let's Play!";
     currentPlayer=O_TEXT;//reset current player 
+    gameLive= true;
+
 };
 restartBtn.addEventListener('click',restart);//listening for click event for restart
 restart();
